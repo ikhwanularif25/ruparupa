08145b7c33bcb6d6175b3369fd6410a3f1c8343d
ikhwanularif25 <lkhwanularif25@gmail.com>
2024-01-30 13:10:07 +0700

    transaksi

--- lib/transaksi.dart
+++ lib/transaksi.dart
@@ -14,18 +14,82 @@ class _transaksiscreenState extends State<transaksiscreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Dashboard"),
+        title: Text(
+          "Pesanan Saya",
+          style: TextStyle(
+            color: Colors.grey,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
         actions: const [],
       ),
       body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(10.0),
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
           child: Column(
             children: [
-              Text(
-                "transaksi",
-                style: TextStyle(
-                  fontSize: 20.0,
+              Container(
+                height: 300,
+                width: 300,
+                decoration: const BoxDecoration(
+                  color: Colors.blue,
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      "https://images.unsplash.com/photo-1533050487297-09b450131914?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              SizedBox(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      Icons.developer_board,
+                      size: 24.0,
+                    ),
+                    Text(
+                      "text",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                        10.0), // Sesuaikan nilai sesuai keinginan Anda
+                    side: BorderSide(color: Colors.blueGrey), // Warna outline
+                  ),
+                  padding: EdgeInsets.zero, // Menghapus padding bawaan tombol
+                ),
+                onPressed: () {},
+                child: SizedBox(
+                  width: double.infinity,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(
+                        Icons.developer_board,
+                        size: 24.0,
+                      ),
+                      const SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        "text",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
