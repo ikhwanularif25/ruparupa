c3ce8d0289ceee660afcc689be5e3940e411d7fb
ikhwanularif25 <lkhwanularif25@gmail.com>
2024-01-30 12:15:29 +0700

    navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,54 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:ruparupa/akun.dart';
+import 'package:ruparupa/beranda.dart';
+import 'package:ruparupa/inspirasi.dart';
+import 'package:ruparupa/transaksi.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyHomePage(),
+    theme: ThemeData(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class MyHomePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
-  }
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
 
+  final List<Widget> _pages = [
+    berandascreen(),
+    inspirasiscreen(),
+    transaksiscreen(),
+    akunscreen(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("aloo...."),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text("Halooo."),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.blue,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'beranda'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.image_search_outlined), label: 'inspirasi'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.source_rounded), label: 'transaksi'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'akun'),
+        ],
       ),
     );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 }
