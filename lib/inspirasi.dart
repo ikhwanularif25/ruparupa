7cb50765add24618246aa4447801c7575bf3c6c2
haruya22222 <annisazulkarnain9@gmail.com>
2024-01-30 15:36:13 +0700

    p

--- lib/inspirasi.dart
+++ lib/inspirasi.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: camel_case_types, prefer_typing_uninitialized_variables
+// ignore_for_file: camel_case_types, prefer_typing_uninitialized_variables, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
