52b5bb81e460227dfa1747d056e29243ef0b83af
ikhwanularif25 <lkhwanularif25@gmail.com>
2024-02-01 17:04:50 +0700

    voucher

--- lib/profil.dart
+++ lib/profil.dart
@@ -254,33 +254,3 @@ class _ProfilState extends State<Profil> {
     );
   }
 }
-// ignore_for_file: camel_case_types, prefer_typing_uninitialized_variables
-
-import 'package:flutter/material.dart';
-
-class Profil extends StatefulWidget {
-  const Profil({Key? key}) : super(key: key);
-
-  @override
-  State<Profil> createState() => _ProfilState();
-}
-
-class _ProfilState extends State<Profil> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Dashboard"),
-        actions: const [],
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [],
-          ),
-        ),
-      ),
-    );
-  }
-}
