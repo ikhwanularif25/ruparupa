a5213a120981726d336620a37b43874a8dd3de67
Haris F <hf130103@gmail.com>
2024-02-01 11:22:59 +0700

    update

--- lib/detail_produk.dart
+++ lib/detail_produk.dart
@@ -2,6 +2,9 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
+import 'keranjang.dart';
+import 'pencarian.dart';
+
 class detailproduk extends StatefulWidget {
   const detailproduk({Key? key}) : super(key: key);
 
@@ -12,6 +15,7 @@ class detailproduk extends StatefulWidget {
 class _CarouselWithIndicatorState extends State<detailproduk> {
   int _current = 0;
   final CarouselController _controller = CarouselController();
+  final TextEditingController _textFieldController = TextEditingController();
   final List<Widget> imgUrls = [
     Container(
       child: Image.asset(
@@ -35,48 +39,126 @@ class _CarouselWithIndicatorState extends State<detailproduk> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Stack(
+      appBar: AppBar(actions: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Column(
-              children: [
-                CarouselSlider(
-                  items: imgUrls,
-                  carouselController: _controller,
-                  options: CarouselOptions(
-                    autoPlay: true,
-                    viewportFraction: 1,
-                    aspectRatio: 3 / 2,
-                    onPageChanged: (index, reason) {
-                      Future.delayed(Duration.zero, () {
-                        setState(() {
-                          _current = index;
-                        });
-                      });
-                    },
+            Container(
+              width: MediaQuery.of(context).size.width * 0.63,
+              height: 42.0,
+              child: TextField(
+                controller: _textFieldController,
+                autocorrect: false,
+                showCursor: true,
+                cursorColor: Colors.amber[700],
+                decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  filled: true,
+                  prefixIcon: InkWell(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => pencarian()));
+                      },
+                      child: Icon(
+                        Icons.search,
+                        color: Colors.orange,
+                      )),
+                  hintText: 'Cari Barang Apa?',
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(
+                      color: Colors.grey,
+                    ),
                   ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(
+                      color: Colors.white, // Warna border saat fokus di sini
+                    ),
+                  ),
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 0, vertical: 0),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: imgUrls.map((url) {
-                    int index = imgUrls.indexOf(url);
-                    return Container(
-                      width: 8.0,
-                      height: 8.0,
-                      margin: EdgeInsets.symmetric(
-                        vertical: 10.0,
-                        horizontal: 2.0,
-                      ),
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: _current == index
-                            ? Colors.blue
-                            : Colors.grey.shade400,
-                      ),
-                    );
-                  }).toList(),
+              ),
+            ),
+            Row(
+              children: [
+                IconButton(
+                  icon: Icon(Icons.share),
+                  color: Colors.black,
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => keranjang()));
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.shopping_cart),
+                  color: Colors.black,
+                  onPressed: () {},
                 ),
               ],
+            ),
+          ],
+        ),
+      ]),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            CarouselSlider(
+              items: imgUrls,
+              carouselController: _controller,
+              options: CarouselOptions(
+                autoPlay: true,
+                viewportFraction: 1,
+                aspectRatio: 3 / 4,
+                onPageChanged: (index, reason) {
+                  Future.delayed(Duration.zero, () {
+                    setState(() {
+                      _current = index;
+                    });
+                  });
+                },
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: Container(
+                padding: EdgeInsetsDirectional.symmetric(
+                  vertical: MediaQuery.of(context).size.width * 0.012,
+                ),
+                width: MediaQuery.of(context).size.width * 0.15,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(8.0),
+                  border: Border.all(
+                    color: Colors.grey,
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "${_current + 1} ",
+                      style: TextStyle(
+                        fontSize: MediaQuery.of(context).size.width * 0.04,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "/ ${imgUrls.length}",
+                      style: TextStyle(
+                        fontSize: MediaQuery.of(context).size.width * 0.04,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
